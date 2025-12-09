99f78ffb7447ecc7ada7434a4893bab4a50fc451
GameXMachine <kalvinator78@gmail.com>
2025-12-09 15:58:28 +0100

    Ajout nouveaux fichiers

--- Labyrinthe_project/src/App.jsx
+++ Labyrinthe_project/src/App.jsx
@@ -3,14 +3,55 @@ import Grid from "./game/Grid";
 import Home from "./pages/Home";
 import Scoreboard from "./pages/Scoreboard";
 import { saveScore } from "./utils/score";
-import "./App.css" ;
+import "./App.css";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("home");
+  const [gameData, setGameData] = useState({
+    level: 1,
+    pseudo: "",
+  });
   const [PlayerPosition, setPlayerPos] = useState({ x: 0, y: 0 });
+  const [scoreData, setScoreData] = useState({
+    isVictory: false,
+    score: 0,
+    pseudo: "",
+  });
+
+  // Navigation vers le jeu 
+  const startGame = (pseudo, level = 1) => {
+    setGameData({ pseudo, level });
+    setCurrentPage("game");
+  };
+
+  // Navigation vers le scoreboard
+  const goToScoreboard = (isVictory, score) => {
+    saveScore(gameData.pseudo, score, isVictory, gameData.level);
+
+    setScoreData({
+      isVictory,
+      score,
+      pseudo: gameData.pseudo,
+    });
+    setCurrentPage("scoreboard");
+  };
+
+  // Rejouer avec le même pseudo et niveau
+  const replay = () => {
+    setCurrentPage("game");
+  };
+
+  const goToHome = () => {
+    setCurrentPage("home");
+    setGameData({ level: 1, pseudo: "" });
+  };
+  
   const gridSize = 10;
 
   useEffect(() => {
     function handleKey(e) {
+      if (currentPage !== "game") return; // N'écouter les touches que pendant le jeu
+      
       setPlayerPos(prev => {
         let { x, y } = prev;
 
@@ -25,31 +66,23 @@ function App() {
 
     window.addEventListener("keydown", handleKey);
     return () => window.removeEventListener("keydown", handleKey);
-  }, []);
+  }, [currentPage]);
 
+  // RENDU CONDITIONNEL ICI
   return (
-    <div style={{ display: "flex", justifyContent: "center", marginTop: "80px" }}>
-      <Grid playerPosition={playerPosition} gridSize={gridSize} />
-    </div>
-  );
-}
-
-return (
-    <div className= "App">
+    <div className="App">
       {currentPage === "home" && (
-        <Home startGame={startGame} />
-        )}
-
-        {currentPage === 'game' && (
-        <Game 
-          level={gameData.level}
-          pseudo={gameData.pseudo}
-          onGameEnd={goToScoreboard}
-        />
+        <Home onStartGame={startGame} />
       )}
 
-      {currentPage === 'scoreboard' && (
-        <Scoreboard 
+      {currentPage === "game" && (
+        <div style={{ display: "flex", justifyContent: "center", marginTop: "80px" }}>
+          <Grid PlayerPosition={PlayerPosition} gridSize={gridSize} />
+        </div>
+      )}
+
+      {currentPage === "scoreboard" && (
+        <Scoreboard
           isVictory={scoreData.isVictory}
           score={scoreData.score}
           pseudo={scoreData.pseudo}
@@ -58,5 +91,7 @@ return (
         />
       )}
     </div>
-);
-export default App
+  );
+}
+
+export default App;
